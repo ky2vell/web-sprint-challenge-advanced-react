8f8e95f329b11e567503e65e421ce2cfec845c58
Kyle Lovell <ky2vell@gmail.com>
2020-06-14 20:19:57 -0400

    checkout form tests added

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,32 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import CheckoutForm from "./CheckoutForm";
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import CheckoutForm from './CheckoutForm';
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test('form header renders', () => {
+  render(<CheckoutForm />);
+  const header = screen.getByText(/checkout form/i);
+  expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test('form shows success message on submit with form details', () => {
+  render(<CheckoutForm />);
+  const firstName = screen.getByLabelText(/first name/i);
+  const lastName = screen.getByLabelText(/last name/i);
+  const address = screen.getByLabelText(/address/i);
+  const city = screen.getByLabelText(/city/i);
+  const state = screen.getByLabelText(/state/i);
+  const zip = screen.getByLabelText(/zip/i);
+  const submit = screen.getByTestId('submit');
+
+  fireEvent.change(firstName, { target: { value: 'Kyle' } });
+  fireEvent.change(lastName, { target: { value: 'Lovell' } });
+  fireEvent.change(address, { target: { value: '1205 Elm St' } });
+  fireEvent.change(city, { target: { value: 'Boise' } });
+  fireEvent.change(state, { target: { value: 'Idaho' } });
+  fireEvent.change(zip, { target: { value: '45742' } });
+  fireEvent.click(submit);
+
+  expect(screen.getByTestId('successMessage')).toBeInTheDocument();
+});
